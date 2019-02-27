897989a509cf9b7530f7599e99b9a72567e220e0
acapulco2059 <somville.benoit@gmail.com>
2019-02-27 00:22:14 -0400

    Fichiers initiaux

---
+++ conf.php
@@ -0,0 +1,12 @@
+<?php
+
+//environnement production
+$GLOBALS["envProd"] = false;
+
+//database connexion
+$GLOBALS["db"] = [
+  "host"     => 'localhost',
+  "user"     => 'root',
+  "password" => 'root',
+  "dataBase" => 'blog_1'
+];
