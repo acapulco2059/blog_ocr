503b536981237e766b83e02526693541f79af601
acapulco2059 <somville.benoit@gmail.com>
2019-02-28 22:49:59 -0400

    Ajout de la page de backend

---
+++ controller/Back.php
@@ -0,0 +1,15 @@
+<?
+
+class back
+{
+  public function getPage($url){
+    $todo = $url[0];                                        //la fonction à appeler par défaut est le premier segment
+    if ($todo == "") $todo = "home";                        //si il n'est pas défini on affiche la page d'accueil
+    if ( !method_exists ( $this, $todo ) ) $todo = "home";  //si la fonction n'existe pas on affiche la page d'accueil
+    return $this->$todo();
+  }
+
+  private function home(){
+     echo "Vous êtes sur la page admin";
+  }
+}
