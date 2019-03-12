d390c958d744784e2b1d6a89cc878a7c80110b85
acapulco2059 <somville.benoit@gmail.com>
2019-03-12 15:12:22 -0400

    Correction d'erreur dans les requêtes SQL

--- model/Model.php
+++ model/Model.php
@@ -1,15 +1,18 @@
 <?php
 
 class Model {
-  private $db;
-  public function __construct() {
-    $this->db = new PDO('mysql:host='.$GLOBALS["db"]["host"].';dbname='.$GLOBALS["db"]["dataBase"].';charset=utf8', $GLOBALS["db"]["user"], $GLOBALS["db"]["password"]);
-    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    if (!$GLOBALS["envProd"]) $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+  private static $db;
+
+  static function init(){
+    self::$db = new PDO('mysql:host='.$GLOBALS["db"]["host"].';dbname='.$GLOBALS["db"]["dataBase"].';charset=utf8', $GLOBALS["db"]["user"], $GLOBALS["db"]["password"]);
+    if (!$GLOBALS["envProd"]) self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    self::$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     unset($GLOBALS["db"]);
   }
 
-  public function select($args){              // build an sql request from args array
+
+  public static function select($args){              // build an sql request from args array
 
     // main things :
     $req  = 'SELECT '.implode(", ", $args["data"]);
@@ -26,49 +29,54 @@ class Model {
     if (isset($args["limit"])) $req .= " LIMIT ".$args["limit"];
 
     // launch request and return result
-    return $this->request($req);
+    return self::request($req);
   }
 
-  public function create($args){
+  public static function create($args){
 
     $req  = 'INSERT INTO ' .$args["into"]."(";
     $req .= implode( " , ", $args["data"]).")";
     $req .= " VALUES (" .implode(" , ", $args["value"]).")";
 
-    return $this->request($req);
+    return self::request($req);
   }
 
-  public function update($args){
+  public static function update($args){
     $req  = 'UPDATE ' .$args['from'];
     $req .= ' SET ' .implode( " , ", $args["data"]."=".$args["value"]);
 
     $req .= ' WHERE ' .$args["where"];
 
-    return $this->request($req);
+    return self::request($req);
   }
 
-  public function delete($args){
+  public static function delete($args){
 
     $req  = 'DELETE FROM ' .$args["from"];
     $req .= ' WHERE ' .$args["where"];
 
-    return $this->request($req);
+    return self::request($req);
     }
 
 
 
-  private function request($sql, $data=NULL) {
+  public static function request($sql, $data=NULL) {
+
+
     try {
       if ($data == NULL) {                     // query
-        $resultat = $this->db->query($sql);
+        $resultat = self::$db->query($sql);
       }
       else {                                   // prepare and execute
-        $resultat = $this->db->prepare($sql);
+        $resultat = self::$db->prepare($sql);
         $resultat->execute($data);
       }
-      $data = $resultat->fetchAll();           //store result
+      $data = $resultat->fetchAll();    //store result
       $resultat->closeCursor();                //close request
-      if (!isset($data[1])) $data=$data[0];    //if there is only one answer we keep it instead of an array
+      if (!isset($data[1])) {
+        if (isset($data[0])) $data=$data[0];   //if there is only one answer we keep it instead of an array
+      }
+
       return [
         "succeed" => TRUE,
         "data"    => $data
